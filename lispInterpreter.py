0e7302acc8862cb8f71e1a7cdb494678f6580d9e
saumya-singh <saumya.singh0993@gmail.com>
2017-12-20 08:59:49 +0530

    included functions to evaluate lambda functions

--- lispInterpreter.py
+++ lispInterpreter.py
@@ -49,11 +49,11 @@ def standardEnv():
 
 global_env = standardEnv()
 
-def eval(x, env ):
+def eval(x, env):
     if isinstance(x, str):
         return env[x]
 
-    elif isinstance(x, int) or isinstance(x, float):
+    elif not isinstance(x, list):
         return x
 
     elif x[0] == 'quote':
@@ -76,14 +76,44 @@ def eval(x, env ):
         (_, symbol, value) = x
         env[symbol] = eval(value, env)
 
+    elif x[0] == 'lambda':
+        (_, params, body) = x
+        return procedure(params, body, env)
+
     else:
         proc = env[x[0]]
         args = [eval(exp, env) for exp in x[1 : ]]
         return proc(*args)
 
+def procedure(parameters, body, env):
 
-if __name__ == '__main__':
-    #print(eval(['begin', ['define', 'r', 10], ['*', 'pi', ['*', 'r', 'r']]], global_env))
+    def callFunction(*args):
+        arg_list = [i for i in args]
+        return eval(body, localEnv(parameters, arg_list, env))
+    return callFunction
+
+collective_env = {}
+
+def localEnv(parameters, arg_list, env):
+    if len(parameters) != len(arg_list):
+        print("number of arguments not equal to the number of parameters")
+        #exit()
+
+    local_env = {}
+    local_var = zip(parameters, arg_list)
+    for var_value_pair in local_var:
+        local_env[var_value_pair[0]] = var_value_pair[1]
+
+    collective_env.update(local_env)
+    collective_env.update(env)
+
+    return collective_env
+    
+
+def main():
+    #ans = eval(['begin', ['define', 'circle-area', ['lambda', ['r'], ['*', 'pi', ['*', 'r', 'r']]]], ['circle-area', ['+', 5, 95]]], global_env)
+    #print(ans)
+
     file_name = argv[1]
     with open(file_name, 'r') as file_obj:
         data = file_obj.read()
@@ -93,3 +123,6 @@ if __name__ == '__main__':
         print(eval(parsed_data[0], global_env))
     else:
         print(parsed_data)
+
+if __name__ == '__main__':
+    main()
